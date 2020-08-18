27ea33ccb000e2807c9b258743893207e2122e87
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2020-08-17 19:04:01 -0700

    new pages

---
+++ public/assets/js/category.js
@@ -0,0 +1,13 @@
+function queryItemsWith(category) {
+  let reference = firebase.database().ref();
+  // prettier-ignore
+  reference.orderByChild("category").equalTo(category).once("value").then(function (snap) {
+    let obj = snap.val()
+
+    for (var item in obj) {
+      // prettier-ignore
+      $("#main").append("<article class='post'>" + obj[item].preview + "</article>");
+    }
+      
+  });
+}
--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -1,10 +1,21 @@
 let articleId = location.search.split("?")[1];
 let contentReference = firebase.database().ref(articleId);
-//   prettier-ignore
-contentReference.child("content").once("value").then(function (snap) {
-  $(".post").append(snap.val());
-});
+
 //   prettier-ignore
 contentReference.child('title').once('value').then(function(snap) {
-  document.title = snap.val()
+  if (snap.val() == null) {
+    document.title = "No Article Found ☹️"
+  } else {
+    document.title = snap.val()
+  }
 })
+
+//   prettier-ignore
+contentReference.child("content").once("value").then(function (snap) {
+  if (snap.val() == null) {
+    $('#preparingarticle').text('No Article Found ☹️')
+  } else {
+    $('#preparingarticle').remove()
+    $(".post").append(snap.val());
+  }
+});
