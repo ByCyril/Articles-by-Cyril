a6129234bcc161475220fe96ed106aa386015ee7
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2020-09-08 02:43:23 -0700

    changes

--- public/assets/js/firebase.js
+++ public/assets/js/firebase.js
@@ -3,8 +3,6 @@ var firebaseConfig = {
   authDomain: "articles-by-cyril.firebaseapp.com",
   databaseURL: "https://articles-by-cyril.firebaseio.com",
   projectId: "articles-by-cyril",
-  storageBucket: "articles-by-cyril.appspot.com",
-  messagingSenderId: "228302831585",
   appId: "1:228302831585:web:f643e53f43a24cf5b75ca0",
 };
 // Initialize Firebase
