5a43e79f91e90472486f72a7f3c01c458f30ed89
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2020-09-16 17:26:17 -0700

    improvements

--- public/assets/js/category.js
+++ public/assets/js/category.js
@@ -4,6 +4,10 @@ function queryItemsWith(category) {
   reference.orderByChild("category").equalTo(category).once("value").then(function (snap) {
     let obj = snap.val()
 
+    if (obj == null) {
+      $("#main").append( "<article class='post'><p id='preparingarticle'>Category is empty at the moment ☹️</p></article>");
+    }
+
     for (var item in obj) {
       // prettier-ignore
       $("#main").append("<article class='post'>" + obj[item].preview + "</article>");
